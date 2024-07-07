574754695bef6d83741552f5028b84e6527a0383
bakrun <noirhn@gmail.com>
2024-07-07 13:04:57 +0900

    commit mesege

---
+++ src/main/java/com/metao/stackitem/Stackitem64.java
@@ -0,0 +1,22 @@
+package com.metao.stackitem;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static net.minecraft.item.Items.BUCKET;
+import static net.minecraft.item.Items.LAVA_BUCKET;
+
+public class Stackitem64 implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("stackitem64");
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/com/metao/stackitem/Stackitem64DataGenerator.java
@@ -0,0 +1,11 @@
+package com.metao.stackitem;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Stackitem64DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
